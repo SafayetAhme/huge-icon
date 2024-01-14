4b540c6a50a102f1c811eeb61ddea081a10f8980
Safayet Ahmed <asafayet21@gmail.com>
2024-01-13 23:36:49 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,86 @@
+import { HugeiconsAllBookmarkStroke, HugeiconsAppleNewsStroke, HugeiconsBubbleChatNotificationStroke, HugeiconsComment01Stroke, HugeiconsEditUser02Stroke, HugeiconsFolderLibraryStroke, HugeiconsHome01Stroke, HugeiconsMessageNotification02Stroke, HugeiconsNewTwitterStroke, HugeiconsNewsStroke, HugeiconsNotificationSnooze01Stroke, HugeiconsNotificationSnooze02Stroke, HugeiconsNotificationSnooze03Stroke, HugeiconsScreenAddToHomeStroke, HugeiconsSearchList02Stroke, HugeiconsUserStroke } from "./hugeicons/hugeicons-react"
+
+
+function App() {
+
+  return (
+    <div className="w-full bg-[#6e6e6e]  p-[200px] justify-center">
+      <div className="bg-black p-[200px]">
+        <div className="bg-white rounded-3xl p-12">
+          <HugeiconsNewTwitterStroke className="mb-6"></HugeiconsNewTwitterStroke>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsHome01Stroke></HugeiconsHome01Stroke></p>
+            <h1 className="text-xl">Home</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsSearchList02Stroke></HugeiconsSearchList02Stroke></p>
+            <h1 className="text-xl">Explore</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsNotificationSnooze03Stroke></HugeiconsNotificationSnooze03Stroke></p>
+            <h1 className="text-xl">Notifications</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsComment01Stroke></HugeiconsComment01Stroke></p>
+            <h1 className="text-xl">Messages</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsFolderLibraryStroke></HugeiconsFolderLibraryStroke></p>
+            <h1 className="text-xl">Lists</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsAllBookmarkStroke></HugeiconsAllBookmarkStroke></p>
+            <h1 className="text-xl">Bookmarks</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsNewsStroke></HugeiconsNewsStroke></p>
+            <h1 className="text-xl">Top Articles</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsUserStroke></HugeiconsUserStroke></p>
+            <h1 className="text-xl">Profile</h1>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white mt-10 p-[200px]">
+      <div className="bg-black text-white rounded-3xl p-12">
+          <HugeiconsNewTwitterStroke className="mb-6"></HugeiconsNewTwitterStroke>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsHome01Stroke></HugeiconsHome01Stroke></p>
+            <h1 className="text-xl">Home</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsSearchList02Stroke></HugeiconsSearchList02Stroke></p>
+            <h1 className="text-xl">Explore</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsNotificationSnooze03Stroke></HugeiconsNotificationSnooze03Stroke></p>
+            <h1 className="text-xl">Notifications</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsComment01Stroke></HugeiconsComment01Stroke></p>
+            <h1 className="text-xl">Messages</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsFolderLibraryStroke></HugeiconsFolderLibraryStroke></p>
+            <h1 className="text-xl">Lists</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsAllBookmarkStroke></HugeiconsAllBookmarkStroke></p>
+            <h1 className="text-xl">Bookmarks</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsNewsStroke></HugeiconsNewsStroke></p>
+            <h1 className="text-xl">Top Articles</h1>
+          </div>
+          <div className="flex mb-4 items-center gap-3">
+            <p><HugeiconsUserStroke></HugeiconsUserStroke></p>
+            <h1 className="text-xl">Profile</h1>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
